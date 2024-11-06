986d5445e04c6f3717536b80d02c7165d93ddb9b
Hong Jing (Jingles) <jinglescode@gmail.com>
2024-11-06 21:09:48 +0800

    add mesh

---
+++ src/pages/mesh/index.tsx
@@ -0,0 +1,15 @@
+import { MeshWallet } from '@meshsdk/core';
+
+export function PageMesh() {
+  function newWallet() {
+    const mnemonic = MeshWallet.brew();
+    console.log('mnemonic', mnemonic);
+  }
+
+  return (
+    <>
+      <h1>mesh demo</h1>
+      <button onClick={() => newWallet()}>new wallet</button>
+    </>
+  );
+}
